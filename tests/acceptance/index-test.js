22089e88a1e85cb84116d7ead7b584e06b1b4f26
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2015-08-01 15:16:46 +0100

    Rework acceptance test for demo purposes
    Move to non-idiomatic testing to allow breakpointing for the demo. Using
    the magic async helper promise means that the test execution order
    doesn't breakpoint well.
    
    Also use position absolute for banners, so that the app renders well in
    the testing container.

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -15,17 +15,15 @@ module('Acceptance | index', {
 });
 
 test('creating alert with demo form', function(assert) {
-  visit('/');
-
-  andThen(() => {
+  visit('/').then(() => {
     assert.equal(find('.alert-banner').length, 0, 'no alerts by default');
   });
 
-  fillIn('input[placeholder="Alert text"]', 'My alert text here');
-  click('input[type=checkbox]:eq(0)');
-  click('button:contains(Create alert)');
-
-  andThen(() => {
+  fillIn('input[placeholder="Alert text"]', 'My alert text here').then(() => {
+    return click('input[type=checkbox]:eq(0)');
+  }).then(() => {
+    return click('button:contains(Create alert)');
+  }).then(() => {
     let alert = find('.alert-banner');
     assert.equal(alert.length, 1, 'alert created');
     assert.equal(alert.text().trim(), 'My alert text here', 'shows text');
