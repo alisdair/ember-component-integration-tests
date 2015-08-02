8a3d946b59a3ed88e46a80b351d2edb83d7e7031
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2015-08-02 01:24:30 +0100

    Add code coverage from ember-cli-blanket
    Try to report to Code Climate via Travis.

---
+++ tests/blanket-options.js
@@ -0,0 +1,33 @@
+// jscs: disable
+/* globals blanket, module */
+
+var options = {
+  modulePrefix: 'component-integration-tests',
+  filter: /^component-integration-tests\//,
+  antifilter: [
+    'component-integration-tests/initializers/export-application-global',
+    'component-integration-tests/initializers/app-version',
+    'component-integration-tests/controllers/array.js',
+    'component-integration-tests/controllers/object.js',
+    '/tests/',
+    '/config/',
+    '/templates/',
+  ],
+  loaderExclusions: [],
+  enableCoverage: true,
+  cliOptions: {
+    lcovOptions: {
+      outputFile: 'lcov.dat',
+      renamer: function(moduleName){
+        var expression = /^component-integration-tests/;
+        return moduleName.replace(expression, 'app') + '.js';
+      }
+    },
+    reporters: ['lcov']
+  }
+};
+if (typeof exports === 'undefined') {
+  blanket.options(options);
+} else {
+  module.exports = options;
+}
